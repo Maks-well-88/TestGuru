b1a1adae357d65f60ee0fd252f4df1e0d4616787
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-08-30 18:17:10 +0300

    Answer model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_151454) do
+ActiveRecord::Schema.define(version: 2021_08_30_151632) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.boolean "correct"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
@@ -29,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_151454) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
