b2794a02a27f75a8a378bf1839292564fa4b3cd6
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-08-31 23:27:49 +0300

    Test class method tests_by_category created

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.tests_by_category(category_title)
+    category = Category.where(title: category_title).first.id
+    Test.where(category_id: category).pluck(:title).sort
+  end
 end
