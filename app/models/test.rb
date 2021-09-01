cd9416f295b64d01ade0523bf9efb06c7e441e81
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-01 13:09:50 +0300

    Refactor tests_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,6 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.tests_by_category(category_title)
-    category = Category.where(title: category_title).first.id
-    Test.where(category_id: category).pluck(:title).sort
+    joins(:category).where('categories.title = ?', category_title).pluck(:title).sort
   end
 end
