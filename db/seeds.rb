1530114c4b1fb496640943a24f83d7c05ec5ae09
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-02 00:24:25 +0300

    Fix: bugs in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Answer.delete_all
 Question.destroy_all
 Test.destroy_all
 Category.destroy_all
 
-  Category.create([{title: 'Промышленность'}, {title: 'IT-сфера'}, {title: 'Общепит'}])
-
-  100.times do |i|
-    test = Test.create(
-          title: Faker::Lorem.sentence(word_count: 3),
-          level: rand(1..3),
-          category_id: rand(1..3))
-
-    user = User.create(
-          name: Faker::Name.name,
-          email: Faker::Internet.email)
-
-    user.tests << test
+  categories = []
+  3.times do
+    categories << Category.create(title: Faker::Lorem.sentence(word_count: 3, supplemental: true))
   end
 
-  100.times do |i|
-    Question.create(
-      body: Faker::Lorem.question(word_count: 4),
-      test_id: i += 1
-      )
+  tests = []
+  categories.each do |category|
+    10.times do
+      tests << Test.create(
+            title: Faker::Lorem.sentence(word_count: 3),
+            level: rand(1..3),
+            category_id: category.id)
+    end
   end
 
-  100.times do |i|
-    Answer.create(
-      body: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
-      correct: [true, false].sample,
-      question_id: i += 1
-      )
+  questions = []
+  tests.each do |test|
+    2.times do
+      questions << Question.create(
+            body: Faker::Lorem.question(word_count: 4),
+            test_id: test.id)
+    end
+    User.create(name: Faker::Name.name, email: Faker::Internet.email).tests << test
+  end
+
+  questions.each do |question|
+    2.times do
+      Answer.create(
+        body: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
+        question_id: question.id)
+    end
   end
 
 p "Создано #{User.count} пользователей."
 p "Создано #{Answer.count} ответов."
-p "Создано #{Category.count} категорий."
+p "Создано #{Category.count} категории."
 p "Создано #{Question.count} вопросов."
-p "Создано #{Test.count} вопросов."
+p "Создано #{Test.count} тестов."
