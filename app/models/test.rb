082007eacf47df4243ab2cbeb3ab85cc7be7cb8d
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-01 20:38:16 +0300

    User instance method tests_by_level created

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :users
 
   def self.tests_by_category(category_title)
     joins(:category).where('categories.title = ?', category_title).pluck(:title).sort
---
+++ db/migrate/20210901171446_add_test_user.rb
@@ -0,0 +1,5 @@
+class AddTestUser < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :tests, :users
+  end
+end
