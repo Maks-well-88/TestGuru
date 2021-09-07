63b4b2c4de37087ff0f976a8dda56be318c3ba3d
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-07 20:42:02 +0300

    Scope methods are defined

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_164549) do
+ActiveRecord::Schema.define(version: 2021_09_07_154710) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
