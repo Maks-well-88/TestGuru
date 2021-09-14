63c500aaca17c922362d0ce617076f87c406124b
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-15 00:40:39 +0300

    Helpers are created

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,4 +2,12 @@ module QuestionsHelper
   def current_year
     Time.now.year
   end
+
+  def github_url(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
+
+  def question_header(question)
+    question.persisted? ? "Edit #{@question.test.title} Question" : "Create New #{@question.test.title} Question"
+  end
 end
