d94f0a33cd8b059aa5798392098c0686b03017e2
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-19 09:25:34 +0300

    Controller for TestPassage created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :test_passages, only: %i[show update] do
+    post :start, on: :member
+  end
+
   resources :tests do
     resources :questions, shallow: true
+      resources :answers, shallow: true
   end
 end
