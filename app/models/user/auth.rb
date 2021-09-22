110758a08decc7780e33b668ec4a4a0380d4f975
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-22 23:27:37 +0300

    Fixed: bug in setter method password

--- app/models/user/auth.rb
+++ app/models/user/auth.rb
@@ -12,7 +12,7 @@ module User::Auth
   def password=(password_string)
     if password_string.nil?
       self.password_digest = nil
-    else
+    elsif password_string.present?
       @password = password_string
       self.password_digest = digest(password_string)
     end
