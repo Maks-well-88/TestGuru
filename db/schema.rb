2ce23dc4d2d00177b2019448460ca4d4de8b9cea
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-22 21:32:04 +0300

    Column password in table users renamed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_221950) do
+ActiveRecord::Schema.define(version: 2021_09_22_182547) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2021_09_18_221950) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "surname"
-    t.string "password"
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
