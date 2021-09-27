877882afb7b5abde88421410f2c1c0e8dd47061d
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-27 19:44:19 +0300

    A greeting for a new user after registration created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
 
-  private
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
 
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Привет, #{current_user.name}! "
     current_user.is_a?(Admin) ? admin_tests_path : tests_path
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :surname, :email, :password)
+    end
+  end
 end
