af1e08905683668b4aad906d4e8dc3aa5fa242ad
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-09-30 15:32:17 +0300

    Localization of tests for the user created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Привет, #{current_user.name}! "
     resource.admin? ? admin_tests_path : tests_path
+  end
+
+  private
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
   def configure_permitted_parameters
