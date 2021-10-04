c709f85f1c264389fc7b746d71121dc7db55466c
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-05 01:05:34 +0300

    Done: API for GitHub Gist

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,6 +1,6 @@
 class TestPassagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test_passage, only: %i[show result update]
+  before_action :set_test_passage, only: %i[show result update gist]
 
   def show
   end
@@ -21,6 +21,19 @@ class TestPassagesController < ApplicationController
     else
       render :show
     end
+  end
+
+  def gist
+    new_gist = GistQuestionService.new(@test_passage.current_question)
+    result = new_gist.call
+
+    flash_options = if new_gist.success?
+      { notice: t('.success', url: result.html_url) }
+    else
+      { alert: t('.failure') }
+    end
+
+    redirect_to @test_passage, flash_options
   end
 
   private
