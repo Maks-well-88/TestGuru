44b581708428829e1f43576e03d37b493afff3ef
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-05 15:03:57 +0300

    Method small_text added into model Gist

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -2,4 +2,10 @@ class Gist < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
+  LENGTH_LIMITATION = 25
+
+  def small_text
+    question.body.truncate(LENGTH_LIMITATION)
+  end
+
 end
