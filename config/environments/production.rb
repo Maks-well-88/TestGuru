88e1e58ee3efbce75e1d3964f4d77fb2f3d7ae9f
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-17 18:15:52 +0300

    Changed port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   	address: 'smtp.gmail.com',
     domain: 'maks-test-guru.herokuapp.com',
-  	port: 587,
+  	port: 465,
   	user_name: ENV['SMTP_USERNAME'],
   	password: ENV['SMTP_PASSWORD'],
   	authentication: :plain,
