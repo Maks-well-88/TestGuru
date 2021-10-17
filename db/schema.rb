7f4456a6cee5fec2afa633719a7db7b9752e905a
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-17 13:57:54 +0300

    Enable_extension "plpgsql"

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_05_100039) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false
