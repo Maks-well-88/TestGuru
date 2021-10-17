0aa0622ce616a1e6cf88158bf5496d5709e18a76
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-17 16:38:30 +0300

    Changed configuration for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'maks-test-guru.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://maks-test-guru.herokuapp.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   	address: 'smtp.gmail.com',
+    domain: 'maks-test-guru.herokuapp.com',
   	port: 587,
   	user_name: ENV['SMTP_USERNAME'],
   	password: ENV['SMTP_PASSWORD'],
