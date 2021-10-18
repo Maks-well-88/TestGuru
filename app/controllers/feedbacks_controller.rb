99299bfba77439c8cf2aae42d05a719fb84e2265
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-18 20:11:52 +0300

    Feedback form added

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,5 +3,20 @@ class FeedbacksController < ApplicationController
   def new; end
 
   def create
+    @feedback = feedback_params
+    if @feedback
+      @feedback[:user] = current_user.email
+
+      FeedbackMailer.feedback(@feedback).deliver_now
+      redirect_to root_path, notice: "Сообщение успешно отправлено!"
+    else
+      render new_feedback_path, alert: "Упс, что-то пошло не так..."
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:title, :body)
   end
 end
